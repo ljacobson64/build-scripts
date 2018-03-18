48130ea2d1120273206f2e18417620e5900ffddc
Lucas John Jacobson <ljacobson64@gmail.com>
2018-03-18 13:31:05 -0500

    Add llvm install script

---
+++ codes/install_llvm.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+function setup_project() {
+  cd ${build_prefix}
+  mkdir -p $1
+  cd $1
+  tarball=$2-${llvm_version}.src.tar.xz
+  url=http://releases.llvm.org/${llvm_version}/${tarball}
+  if [ ! -f ${dist_dir}/llvm/${tarball} ]; then wget ${url} -P ${dist_dir}/llvm/; fi
+  tar -xJvf ${dist_dir}/llvm/${tarball}
+  mv $2-${llvm_version}.src $3
+}
+
+set -e
+
+build_prefix=${build_dir}/llvm-${llvm_version}
+install_prefix=${install_dir}/llvm-${llvm_version}
+
+rm -rf ${build_prefix}
+mkdir -p ${build_prefix}/bld
+
+setup_project .                      llvm              llvm
+setup_project llvm/tools             cfe               clang
+setup_project llvm/tools             lld               lld
+setup_project llvm/tools             polly             polly
+setup_project llvm/tools/clang/tools clang-tools-extra extra
+setup_project llvm/projects          compiler-rt       compiler-rt
+setup_project llvm/projects          libcxx            libcxx
+setup_project llvm/projects          libcxxabi         libcxxabi
+setup_project llvm/projects          libunwind         libunwind
+setup_project llvm/projects          openmp            openmp
+setup_project llvm/projects          test-suite        test-suite
+
+cd ${build_prefix}
+ln -s llvm src
+cd bld
+
+cmake_string=
+cmake_string+=" -DCMAKE_BUILD_TYPE=Release"
+cmake_string+=" -DCMAKE_C_COMPILER=${CC}"
+cmake_string+=" -DCMAKE_CXX_COMPILER=${CXX}"
+cmake_string+=" -DCMAKE_INSTALL_PREFIX=${install_prefix}"
+if [ -n "${compiler_lib_dirs}" ]; then
+  cmake_string+=" -DCMAKE_INSTALL_RPATH=${compiler_lib_dirs}"
+fi
+
+cmake ../src ${cmake_string}
+make -j${jobs}
+${sudo_cmd} make install
