eda0f2468656432b07a7e6d1f1fcf20efdf61169
Lucas John Jacobson <ljacobson64@gmail.com>
2018-03-28 16:48:53 -0500

    Correctly add gfortran path when building fluka

--- codes/install_fluka.sh
+++ codes/install_fluka.sh
@@ -10,7 +10,7 @@ cd ${install_prefix}/bin
 tarball=fluka${fluka_version}-linux-gfor64bitAA.tar.gz
 ${sudo_cmd} tar -xzvf ${dist_dir}/misc/${tarball}
 
-if [ -z "${gcc_dir}" ]; then PATH=${gcc_dir}/bin:${PATH}; fi
+if [ ! -z "${gcc_dir}" ]; then PATH=${gcc_dir}/bin:${PATH}; fi
 export FLUFOR=$(basename $FC)
 export FLUPRO=${PWD}
 
