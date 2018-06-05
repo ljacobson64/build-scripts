022c79f7aa8e1c7445f9f88a989bd52d44c96899
Lucas John Jacobson <ljacobson64@gmail.com>
2018-06-05 13:09:09 -0500

    Add build script for mcnp2cad built against CGM-OCE

---
+++ codes/install_mcnp2cad_oce.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+export cgm_version=16.0
+
+build_prefix=${build_dir}/mcnp2cad-cgm-${cgm_version}-oce-${oce_version}
+install_prefix=${install_dir}/mcnp2cad-cgm-${cgm_version}-oce-${oce_version}
+
+armadillo_dir=${install_dir}/armadillo-${armadillo_version}
+oce_dir=${native_dir}/oce-${oce_version}
+cgm_dir=${install_dir}/cgm-${cgm_version}-oce-${oce_version}
+
+repo=https://github.com/svalinn/mcnp2cad
+branch=master
+
+rm -rfv ${build_prefix}
+mkdir -pv ${build_prefix}
+cd ${build_prefix}
+git clone ${repo} -b ${branch} --single-branch
+cd mcnp2cad
+sed -i 's/LDFLAGS = ${IGEOM_LIBS}/LDFLAGS += ${IGEOM_LIBS}/' Makefile
+
+make_string=
+if [ ! -f /usr/lib/libarmadillo.so ]; then
+  make_string+=" ARMADILLO_BASE_DIR=${armadillo_dir}"
+fi
+make_string+=" CGM_BASE_DIR=${cgm_dir}"
+make_string+=" CC=${CC} CXX=${CXX} FC=${FC}"
+make_string_pre="LDFLAGS=-Wl,-rpath,${compiler_lib_dirs}:${oce_dir}/bin:${cgm_dir}/lib"
+
+eval ${make_string_pre} make -j${jobs} ${make_string}
+${sudo_cmd} mkdir -pv ${install_prefix}/bin
+${sudo_cmd} cp -pv mcnp2cad ${install_prefix}/bin
