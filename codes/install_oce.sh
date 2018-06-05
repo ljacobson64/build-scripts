7539585d29ca3e2a11b9be65ecea7da6bd9f6fb0
Lucas John Jacobson <ljacobson64@gmail.com>
2018-06-05 12:05:20 -0500

    Add OCE build script

---
+++ codes/install_oce.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+build_prefix=${build_dir}/oce-${oce_version}
+install_prefix=${install_dir}/oce-${oce_version}
+
+rm -rfv ${build_prefix}
+mkdir -pv ${build_prefix}/bld
+cd ${build_prefix}
+tarball=OCE-${oce_version}.tar.gz
+url=https://github.com/tpaviot/oce/archive/${tarball}
+if [ ! -f ${dist_dir}/misc/${tarball} ]; then wget ${url} -P ${dist_dir}/misc/; fi
+tar -xzvf ${dist_dir}/misc/${tarball}
+ln -sv oce-OCE-${oce_version} src
+cd bld
+
+cmake_string=
+cmake_string+=" -DCMAKE_BUILD_TYPE=Release"
+cmake_string+=" -DCMAKE_C_COMPILER=${CC}"
+cmake_string+=" -DCMAKE_CXX_COMPILER=${CXX}"
+cmake_string+=" -DOCE_INSTALL_PREFIX=${install_prefix}"
+#cmake_string+=" -DCMAKE_INSTALL_RPATH=${compiler_lib_dirs}:${install_prefix}/lib"
+
+cmake ../src ${cmake_string}
+make -j${jobs}
+${sudo_cmd} make install
