90a766ca41dc823b83b7aea76d6165dfc7bf9aa0
Lucas John Jacobson <ljacobson64@gmail.com>
2019-08-16 10:56:03 -0500

    Add cnpy

---
+++ codes/install_cnpy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+build_prefix=${build_dir}/cnpy
+install_prefix=${install_dir}/cnpy
+
+rm -rfv ${build_prefix}
+mkdir -pv ${build_prefix}/bld
+cd ${build_prefix}
+git clone https://github.com/rogersce/cnpy -b master --single-branch
+ln -sv cnpy src
+cd bld
+
+cmake_string=
+cmake_string+=" -DCMAKE_BUILD_TYPE=Release"
+cmake_string+=" -DCMAKE_CXX_COMPILER=${CXX}"
+cmake_string+=" -DCMAKE_INSTALL_PREFIX=${install_prefix}"
+cmake_string+=" -DCMAKE_INSTALL_RPATH=${compiler_lib_dirs}:${install_prefix}/lib"
+
+${CMAKE} ../src ${cmake_string}
+make -j${jobs}
+${sudo_cmd_install} make -j${jobs} install
