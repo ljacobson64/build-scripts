384e851c84ca7e816e126f007ca6f23739b48e8c
Lucas John Jacobson <ljacobson64@gmail.com>
2020-04-26 15:38:20 -0500

    Update versions

--- codes/install_fluka.sh
+++ codes/install_fluka.sh
@@ -8,7 +8,7 @@ ${sudo_cmd_install} rm -rfv ${install_prefix}
 ${sudo_cmd_install} mkdir -pv ${install_prefix}/bin
 cd ${install_prefix}/bin
 tarball=fluka${fluka_version}-linux-gfor64bitAA.tar.gz
-${sudo_cmd_install} tar -xzvf ${dist_dir}/misc/${tarball}
+${sudo_cmd_install} tar -xzvf ${dist_dir}/fluka/${tarball}
 
 if [ ! -z "${gcc_dir}" ]; then PATH=${gcc_dir}/bin:${PATH}; fi
 
