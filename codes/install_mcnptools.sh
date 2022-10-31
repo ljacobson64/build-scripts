8af16eefcd60dfd9bac13334a0b7e28ee3b2eb0e
Lucas John Jacobson <ljacobson64@gmail.com>
2022-10-31 09:20:52 -0500

    Add ISC and update MCNPTOOLS

--- codes/install_mcnptools.sh
+++ codes/install_mcnptools.sh
@@ -8,27 +8,26 @@ install_prefix=${install_dir}/MCNPTOOLS-${mcnptools_version}
 rm -rfv ${build_prefix}
 mkdir -pv ${build_prefix}/bld
 cd ${build_prefix}
-unzip ${dist_dir}/mcnp/mcnptools-${mcnptools_version}.zip
-ln -sv Source/libmcnptools src
+git clone https://github.com/lanl/mcnptools -b main
+cd mcnptools
+git checkout v${mcnptools_version}
+cd ..
+ln -sv mcnptools src
 cd bld
 
-cmake_string=
-cmake_string+=" -DCMAKE_BUILD_TYPE=Release"
-cmake_string+=" -DCMAKE_C_COMPILER=${CC}"
-cmake_string+=" -DCMAKE_CXX_COMPILER=${CXX}"
-cmake_string+=" -DCMAKE_INSTALL_PREFIX=${install_prefix}"
+cmake_string_1=
+cmake_string_1+=" -DCMAKE_BUILD_TYPE=Release"
+cmake_string_1+=" -DCMAKE_C_COMPILER=${CC}"
+cmake_string_1+=" -DCMAKE_CXX_COMPILER=${CXX}"
+cmake_string_1+=" -DCMAKE_INSTALL_PREFIX=${install_prefix}"
+cmake_string_2=${cmake_string_1}
+cmake_string_1+=" -Dmcnptools.python_install=Prefix"
+cmake_string_2+=" -Dmcnptools.python_install=User"
 
-${CMAKE} ../src ${cmake_string}
+${CMAKE} ../src ${cmake_string_1}
 make -j${num_cpus}
 make -j${num_cpus} install
 
-cd ../Source/python
-
-mcnptools_pythonpath=${install_prefix}/lib/python${python3_version_major}/site-packages
-python3 setup.py build
-mkdir -pv ${mcnptools_pythonpath}
-PYTHONPATH=${mcnptools_pythonpath}
-python3 setup.py install --prefix=${install_prefix}
-d=$(cat ${mcnptools_pythonpath}/easy-install.pth)
-d=$(basename ${d})
-ln -sv ${d}/mcnptools ${mcnptools_pythonpath}/mcnptools
+${CMAKE} ../src ${cmake_string_2}
+make -j${num_cpus}
+make -j${num_cpus} install
