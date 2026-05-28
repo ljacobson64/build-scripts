05ecdc8cf9af3053caa59ea76ab38f893d284bf0
Lucas John Jacobson <ljacobson64@gmail.com>
2026-05-28 11:51:47 -0500

    Build acetk with install rpath

--- codes/install_acetk.sh
+++ codes/install_acetk.sh
@@ -18,10 +18,10 @@ cmake_string+=" -DCMAKE_CXX_COMPILER=${CXX}"
 cmake_string+=" -DCMAKE_INSTALL_PREFIX=${install_prefix}"
 if [ -n "${compiler_rpath_dirs}" ]; then
   cmake_string+=" -DCMAKE_INSTALL_RPATH=${compiler_rpath_dirs}"
+  sed -i "s/ INSTALL_RPATH \"/ INSTALL_RPATH \"\${CMAKE_INSTALL_RPATH}:/" ../ACEtk/python/CMakeLists.txt
 fi
 
 ${CMAKE} ../src ${cmake_string}
 make -j${num_cpus}
 make -j${num_cpus} install
-mkdir -pv ${install_prefix}/lib
-cp -pv python/ACEtk.cpython-${python_version_major/.}-x86_64-linux-gnu.so ${install_prefix}/lib/
+ln -sv ${install_prefix}/lib/python${python_version_major}/site-packages/ACEtk.cpython-${python_version_major/.}-x86_64-linux-gnu.so ${install_prefix}/lib/
